997d6fa66f2bd3cfe4f703824d9fef41c3c9c61f
gal.esm49 <esm.2001@mail.ru>
2025-02-26 19:08:30 +0500

    Update App.jsx

--- homeworks/portfolio/portfolio/src/App.jsx
+++ homeworks/portfolio/portfolio/src/App.jsx
@@ -5,18 +5,25 @@ import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Footer from './components/Footer';
+import "./App.css";
 
 const App = () => {
   return (
+  <div className="app-container">
     <Router>
       <Navbar />
+      <div className="content">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      </div>
+      <Footer />
     </Router>
+  </div>
   );
 };
 
